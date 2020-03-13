05400c1b6230bc107fd308e32cfba3715a975ad5
Arden Allen <39927658+ardenallen@users.noreply.github.com>
2020-03-12 22:49:54 -0700

    mute working?

---
+++ InstructorEnd/Assets/Script/Buttons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    private Image im;
+    private Color enabled;
+    private Color disabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        im = this.transform.Find("Image").GetComponent<Image>();
+        disabled = new Color(0.4196078f, 0.4196078f, 0.4196078f, 1.0f); 
+        enabled = new Color(0.1803922f, 0.7686275f, 0.6509804f, 1.0f);
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        if (im.color != enabled)
+        {
+            im.color = enabled;
+        }
+        else
+        {
+            im.color = disabled;
+        }
+    }
+}
---
+++ InstructorEnd/Assets/Script/color_change.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class color_change : MonoBehaviour
+{
+    public int upperLimit;
+    public int lowerLimit;
+    private Text number;
+    //public Color normal;
+    private Color normal;
+
+    // Update is called once per frame
+    void Start()
+    {
+        number = this.transform.Find("Number").GetComponent<Text>();
+        normal = number.color;
+    }
+
+    void Update()
+    {
+        int value = int.Parse(number.text);
+        if (value > upperLimit || value < lowerLimit)
+        {
+            number.color = Color.red;
+        }
+        else
+        {
+            number.color = normal;
+        }
+
+    }
+}

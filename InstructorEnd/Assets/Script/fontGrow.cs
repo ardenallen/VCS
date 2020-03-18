9be9eeef86ba56b9e2bfb19281b7aec14e190081
Arden Allen <39927658+ardenallen@users.noreply.github.com>
2020-03-18 14:28:58 -0700

    minor changes

---
+++ InstructorEnd/Assets/Script/fontGrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class fontGrow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = transform.Find("Number").GetComponent<Text>(); 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.fontSize += 10;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.fontSize -= 10;
+    }
+}

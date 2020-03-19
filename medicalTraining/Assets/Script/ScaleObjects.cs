3543500322f4571035179f2fc7c19270bd5d943c
Emily <sakuya0116@hotmail.com>
2020-03-18 20:38:49 -0700

    backup

---
+++ medicalTraining/Assets/Script/ScaleObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleObjects : MonoBehaviour
+{
+    public OVRGrabbable grabbable;
+
+    public bool scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabbable = transform.GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+  
+}
